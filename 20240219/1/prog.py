e8a503877feaeafa031cb04052d2e26c71543f6b
root <root@LAPTOP-T55L26QF>
2024-02-23 16:17:43 +0300

    4

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -5,14 +5,25 @@ import zlib
 
 
 
-
+def show_tree(tree):
+	tree_workfile = os.path.join(".git/objects", tree[0:2], tree[2:])
+	with open(tree_workfile, "rb") as f:
+		data = zlib.decompress(f.read())
+	header, _, body = data.partition(b'\x00')
+	while body:
+		treeobj, _, tail = body.partition(b"\x00")
+		num, body = tail[:20], tail[20:]
+		tmode, tname = treeobj.split()
+		num, body = tail[:20], tail[20:]
+		obj_type = "tree" if list(glob.iglob(os.path.join(".git/objects", num.hex()[0:2], num.hex()[2:]))) else "blob"
+		print(f"{obj_type} {num.hex()}\t{tname.decode()}")
 		
 
 
 
 def show_commit(commit):
-	commit_file = os.path.join(".git/objects", commit[0:2], commit[2:])
-	with open(commit_file, "rb") as f:
+	commit_workfile = os.path.join(".git/objects", commit[0:2], commit[2:])
+	with open(commit_workfile, "rb") as f:
 		data = zlib.decompress(f.read())
 	tree = b"\n".join(data.split(b"\n")[0:1]).decode().split("tree")[1].strip()
 	parent = b"\n".join(data.split(b"\n")[1:2]).decode().split("parent")[1].strip()
@@ -27,13 +38,13 @@ def in_objs(hash):
 
 
 if len(sys.argv) == 1:
-	for file in glob.iglob(".git/refs/heads/*"):
-		print(file.split(os.sep)[-1])
+	for workfile in glob.iglob(".git/refs/heads/*"):
+		print(workfile.split(os.sep)[-1])
 else:
-	file = glob.iglob(os.path.join(".git/refs/heads", sys.argv[1])).__next__()
-	with open(file, "r") as f:
+	workfile = glob.iglob(os.path.join(".git/refs/heads", sys.argv[1])).__next__()
+	with open(workfile, "r") as f:
 		parent = f.read().split()[0]
 
 	while in_objs(parent):
 		tree, parent = show_commit(parent)
-		#show_tree(tree)
+		show_tree(tree)
