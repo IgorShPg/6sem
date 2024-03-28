3c20f997bd3097027b5b6dae4d028bcc09dc8427
root <root@LAPTOP-T55L26QF>
2024-03-28 18:47:52 +0300

    Make server

--- 20240325/1/server.py
+++ 20240325/1/server.py
@@ -7,6 +7,8 @@ import asyncio
 pos=0, 0
 monster_position={}
 custom_monsters={"jgsbat"}
+clients={}
+logins={}
 weapons={"sword" : 10, "spear" : 15, "axe" : 20}
 
 
@@ -16,22 +18,24 @@ def move(x, y):
     return f"Moved to {pos}"
 
 
-def addmon(x, y, name, hello, hp):
+def addmon(x, y, name, hello, hp, gamer):
     ans=[]
     if x>9 or x<0 or y>9 or y<0:
         ans.append("Invalid arguments")
     else:
         if name in cowsay.list_cows() or name in custom_monsters:
-            global monster_position
+            global monster_position,logins
+            flag=True
             replace=monster_position.get((x,y), None)
             monster_position[(x,y)]=[name, hello, hp]
-            ans.append(f"Added monster {name} with {hp} hitpoints to {x, y} saying {hello}")
+            ans.append(f"{logins[gamer]} added monster {name} with {hp} hitpoints to {x, y} saying {hello}")
             if replace:
-                ans.append("Replaced the old monster")
+                ans.append(f"{logins[gamer]} Replaced the old monster")
         else:
             ans.append("Cannot add unknown monster")
+            flag=False
 
-    return "\n".join(ans)
+    return "\n".join(ans), flag
 
 
 
@@ -39,16 +43,21 @@ def encounter():
     global pos, monster_position
     if mon:=monster_position.get(pos, None):
         name, hello=mon[0], mon[1]
-        return f"COW {name} {hello}"
+        if name in custom_monsters:
+            return cowsay.cowsay(hello, cowfile=custom_monsters[name])
+        else:
+            return cowsay.cowsay(hello, cow=name)
+
 
     return None
 
-def attack(name,damage):
-    global pos, monster_position
+def attack(name,damage,gamer):
+    global pos, monster_position,logins
     ans=[]
+    flag=True
     if (mon := monster_position.get(pos, None)) and mon[0] == name:
         damage = damage if mon[2] >= damage else mon[2]
-        ans.append(f"Attacked {mon[0]},  damage {damage} hp")
+        ans.append(f"{logins[gamer]} attacked {mon[0]},  damage {damage} hp")
         monster_position[pos][2]-=damage
         if monster_position[pos][2]:
             ans.append(f"{mon[0]} now has {monster_position[pos][2]}")
@@ -57,29 +66,87 @@ def attack(name,damage):
             del monster_position[pos]
     else:
         ans.append(f"No {name} here")
-    return "\n".join(ans)
+        flag=False
+    return "\n".join(ans),flag
+
+
+def login(me, nickname):
+    global logins
+    if nickname in logins:
+        return "This login is already taken", False
+    else:
+        logins[me] = nickname
+        return f"You logged in as {nickname}", True
 
 
 async def cowhandler(reader, writer):
-    host, port = writer.get_extra_info('peername')
+    me = "{}:{}".format(*writer.get_extra_info('peername'))
+    print(me)
+    clients[me] = asyncio.Queue()
+    send = asyncio.create_task(reader.readline())
+    receive = asyncio.create_task(clients[me].get())
     while not reader.at_eof():
-        request = (await reader.readline()).strip().decode()
-        match request.split():
-            case ["move", x, y]:
-                ans = [move(int(x), int(y))]
-                if enc := encounter():
-                    ans.append(enc)
-                ans = "\n".join(ans)
-            case ["addmon", x, y, name, hp, *hello]:
-                hello = " ".join(hello)
-                ans = addmon(int(x), int(y), name, hello, int(hp))
-            case ["attack", name, damage]:
-                ans = attack(name, int(damage))
+        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        for q in done:
+            if q is send:
+                send = asyncio.create_task(reader.readline())
+                match q.result().decode().split():
+                    case ["login", nickname]:
+                        login_msg, login_info = login(me, nickname)
+                        print(clients, logins)
+                        await clients[me].put(login_msg)
+                        if login_info:
+                            for client in clients:
+                                await clients[client].put(f"{nickname} has connected to MUD")
 
-        writer.write(ans.encode())
+                    case ["move", x, y]:
+                        if me in logins:
+                            ans = [move(int(x), int(y))]
+                            if enc := encounter():
+                                ans.append(enc)
+                            await clients[me].put("\n".join(ans))
+
+                    case ["addmon", x, y, name, hp, *hello]:
+                        if me in logins:
+                            hello = " ".join(hello)
+                            msg, broadcast = addmon(int(x), int(y), name, hello, int(hp), me)
+                            if broadcast:
+                                for client in clients:
+                                    await clients[client].put(msg)
+                            else:
+                                await clients[me].put(msg)
+
+                    case ["attack", name, damage]:
+                        if me in logins:
+                            msg, broadcast = attack(name, int(damage), me)
+                            if broadcast:
+                                for client in clients:
+                                    await clients[client].put(msg)
+                            else:
+                                await clients[me].put(msg)
+
+                    case ["exit", ]:
+                        if me in logins:
+                            await clients[me].put("exit")
+                            for client in clients:
+                                if client != me:
+                                    await clients[client].put(f"{logins[me]} has disconnected from MUD")
+                            receive.cancel()
+                            writer.close()
+                            await writer.wait_closed()
+
+                            del logins[me]
+                            del clients[me]
+
+            elif q is receive:
+                receive = asyncio.create_task(clients[me].get())
+                writer.write(f"{q.result()}\n".encode())
+                await writer.drain()
+
 
     writer.close()
     await writer.wait_closed()
+
 
 
 
@@ -91,3 +158,4 @@ async def main():
         await server.serve_forever()
 
 asyncio.run(main())
+
